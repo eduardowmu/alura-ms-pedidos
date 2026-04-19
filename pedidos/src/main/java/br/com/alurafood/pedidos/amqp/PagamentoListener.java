e4c2cbe1b69bf185cd8a6b4cca98251274f60467
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-04-19 10:16:23 -0300

    recebendo mensagens de pagamentos

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,13 +1,24 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoListener {
     @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message mensagem) {
-        System.out.println("Recebi a mensagem " + mensagem.toString());
+    public void recebeMensagem(PagamentoDto pagamentoDto) {
+        String mensagem = """
+			Dados do pagamento: %s
+           Número do pedido: %s
+           Valor R$: %s
+           Status: %s 
+                """.formatted(
+                pagamentoDto.getId(),
+                pagamentoDto.getPedidoId(),
+                pagamentoDto.getValor(),
+                pagamentoDto.getStatus());
+
+        System.out.println("Recebi a mensagem " + mensagem);
     }
 }
